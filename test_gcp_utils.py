b2d62689b02882d239c33e154d1b0702ecb6a110
SwayamY <swayam.science777@gmail.com>
2025-06-10 16:17:57 +0530

    chore: credentail issue fixed

--- test_gcp_utils.py
+++ test_gcp_utils.py
@@ -1,6 +1,8 @@
 # test_gcp_utils.py
 from app.utils.gcp_utils import create_instance , get_instance_external_ip
 import time
+import os
+
 
 if __name__ == "__main__":
     TEST_REPO = "https://github.com/SwayamY/sample-docker-multicontainer-for-testing.git"
